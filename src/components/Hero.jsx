e469dae75bedac521f4a932799b50ad7b6f6e4f8
owen-wang-student <owang420@gmail.com>
2024-04-26 23:15:34 -0500

    test padding

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,8 @@ import { ComputersCanvas } from './canvas'
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto xxl:mt-10">
-      <div className={`${styles.padding} max-w-7xl mx-auto absolute z-0 inset-0 xs:top-[50px] top-[120px] flex flex-row items-start gap-5`}>
+    <section className="relative w-full h-screen mx-auto">
+      <div className={`${styles.padding} max-w-7xl mx-auto absolute z-0 inset-0 xs:top-[50px] top-[120px] xxl:top-[250px] flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
