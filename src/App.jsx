6809d9d2c0baaef52b44e24c7ea131af5a7d2fbb
owen-wang-student <owang420@gmail.com>
2024-05-08 23:52:33 -0500

    added loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,40 @@
 import { BrowserRouter } from 'react-router-dom'
-import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
+import { useState, useEffect } from 'react'
+import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas, LoadingScreen } from './components'
 
 function App() {
+
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 5000)
+    console.log(isLoading)
+  }, [])
+
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar />
-          <Hero />
-        </div>  
-        <div>
-          <About />
-          <Experience />
-          <Tech />
-          <Works />
+      {isLoading ? (
+        <LoadingScreen />
+      ) : (
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Navbar />
+            <Hero />
+          </div>  
+          <div>
+            <About />
+            <Experience />
+            <Tech />
+            <Works />
+          </div>
+          <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+          </div>
         </div>
-        <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
-        </div>
-
-      </div>
+      )}
     </BrowserRouter>
   )
 }
