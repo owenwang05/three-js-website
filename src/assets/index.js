5e59fbaa026dfb550574288b054f43ab3f4421fe
owen-wang-student <owang420@gmail.com>
2024-05-15 01:03:34 -0500

    imported link image

--- src/assets/index.js
+++ src/assets/index.js
@@ -31,6 +31,8 @@ import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
+import link from "./link.png"
+
 export {
   logo,
   backend,
@@ -61,4 +63,5 @@ export {
   carrent,
   jobit,
   tripguide,
+  link
 };
