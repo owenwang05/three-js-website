4e0d60bab73b8d68499d0ecbc46fc2bac4314129
owen-wang-student <owang420@gmail.com>
2024-05-14 23:27:14 -0500

    added hero load animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
 import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas, LoadingScreen } from './components'
 
 function App() {
 
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true) // remember to change to true!!?!?!?!?
 
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false)
-    }, 5000)
-    console.log(isLoading)
+    }, 8000)
   }, [])
 
   return (
@@ -19,10 +19,12 @@ function App() {
         <LoadingScreen />
       ) : (
         <div className="relative z-0 bg-primary">
-          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <motion.div 
+            className="bg-hero-pattern bg-cover bg-no-repeat bg-center"
+          >
             <Navbar />
             <Hero />
-          </div>  
+          </motion.div>  
           <div>
             <About />
             <Experience />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import { item, container } from '../utils/motion'
 import { styles } from '../styles'
 import { ComputersCanvas } from './canvas'
 
@@ -7,23 +8,45 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto block items-center justify-center">
       <div className="w-full xxxl:h-[1400px] h-screen">
-        <div className="flex flex-col w-full h-full">
-          <div className={`${styles.padding} max-w-7xl xxl:max-w-[1536px] mx-auto absolute z-0 inset-0 xs:top-[65px] top-[120px] xxl:top-[130px] xxxl:top-[180px] flex flex-row items-start gap-5`}>
-            <div className="flex flex-col justify-center items-center mt-5">
+        <motion.div variants={container(2)} initial="hidden" animate="show" className="flex flex-col w-full h-full">
+          <motion.div 
+            variants={container(0)}
+            initial="hidden"
+            animate="show"
+            className={`${styles.padding} max-w-7xl xxl:max-w-[1536px] mx-auto absolute z-0 inset-0 xs:top-[65px] top-[120px] xxl:top-[130px] xxxl:top-[180px] flex flex-row items-start gap-5`}
+          >
+            {/* contains side bar */}
+            <motion.div variants={item(10, "pos")} className="flex flex-col justify-center items-center mt-5">
               <div className="w-5 h-5 rounded-full bg-[#915eff]" />
               <div className="w-1 sm:h-80 h-40 violet-gradient" />
-            </div>
-    
-            <div>
-              <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Owen</span></h1>
-              <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            </motion.div>
+
+            {/* contains hero text */}
+            <motion.div
+              variants={container(0)}
+              initial="hidden"
+              animate="show"
+            >
+              <motion.h1 
+                variants={item(15, "pos")}
+                className={`${styles.heroHeadText} text-white`}
+              >
+                Hi, I'm <span className="text-[#915eff]">Owen</span>
+              </motion.h1>
+              <motion.p 
+                variants={item(15, "pos")}
+                className={`${styles.heroSubText} mt-2 text-white-100`}
+              >
                 I develop web applications that <br className="sm:block hidden "/>address real-life problems
-              </p>
-            </div>
-          </div>
+              </motion.p>
+            </motion.div>
+          </motion.div>
           
-          <ComputersCanvas />
-        </div>
+          {/* contains computer */}
+          <motion.div initial={{opacity: 0}} animate={{opacity:1}} transition={{delay: 1, duration: 2.5}}className="w-full h-full">
+            <ComputersCanvas />
+          </motion.div>
+        </motion.div>
       </div>
     </section>
   )
