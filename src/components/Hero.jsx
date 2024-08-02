0e6aae9d5e2ec9d2287213451fc61ca8e63db38a
owen-wang-student <owang420@gmail.com>
2024-08-02 15:39:39 -0400

    fixed hero text loading issue

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,7 +34,7 @@ const Hero = () => {
             {/* contains side bar */}
             <motion.div 
               variants={isMobile ? null : item(15, "pos")} 
-              initial={isMobile ? {opacity:1} : "hidden"}
+              initial={isMobile ? {opacity:1} : {opacity:0, y:15}}
               className={`flex flex-col justify-center items-center ${isMobile ? "" : "mt-5"}`}
             >
               <div className="w-5 h-5 rounded-full bg-[#915eff]" />
@@ -45,14 +45,14 @@ const Hero = () => {
             <motion.div>
               <motion.h1 
                 variants={isMobile ? null : item(15, "pos")} 
-                initial={isMobile ? {opacity:1} : "hidden"}
+                initial={isMobile ? {opacity:1} : {opacity:0, y:15} }
                 className={`${styles.heroHeadText} text-white`}
               >
                 Hi, I'm <span className="bg-gradient-to-l from-[#8359df] to-[#8150ea] via-[#cbb7ef] via-45% text-transparent bg-clip-text">Owen</span>
               </motion.h1>
               <motion.p 
                 variants={isMobile ? null : item(15, "pos")} 
-                initial={isMobile ? {opacity:1} : "hidden"}
+                initial={isMobile ? {opacity:1} : {opacity:0, y:15} }
                 className={`${styles.heroSubText} mt-2 text-white-100`}
               >
                 I develop web applications that <br className="sm:block hidden "/>address <span className="bg-gradient-to-l from-[#8359df] to-[#d6c7f2] via-[#d6c7f2] via-15% text-transparent bg-clip-text">real-world</span> problems
